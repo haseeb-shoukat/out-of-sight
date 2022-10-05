3c09cfeb2a326fbdcf0bc3d217a15a4e3b31410f
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-10-06 04:31:56 +0500

    Add classNames for level cards/images

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ const Home = () => {
     <div className="home">
       <div className="main-heading">Out Of Sight</div>
       <div className="wave">
+        <div className="secondary-heading">Choose your Battlegrounds</div>
         <svg viewBox="0 0 500 100" preserveAspectRatio="none">
           <path
             d="M-70.02,-25.05 C168.68,72.62 271.49,-49.98 542.84,29.20 L500.00,0.00 L-40.11,-71.44 Z"
@@ -17,16 +18,20 @@ const Home = () => {
       </div>
       <div className="levels">
         <div className="level-card">
-          <img src={post} />
+          <img src={robo} className="level-img" />
+          <div className="level-name">Robo City</div>
         </div>
         <div className="level-card">
-          <img src={robo} />
+          <img src={post} className="level-img" />
+          <div className="level-name">Post Apocalypse</div>
         </div>
         <div className="level-card">
-          <img src={space} />
+          <img src={underground} className="level-img" />
+          <div className="level-name">Underground Base</div>
         </div>
         <div className="level-card">
-          <img src={underground} />
+          <img src={space} className="level-img" />
+          <div className="level-name">Space Frenzy</div>
         </div>
       </div>
     </div>
