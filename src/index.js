7aa273be3e73828f9bc282b9345493cabd33159e
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-10-05 21:07:23 +0500

    Add Home component to app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/index.css";
+import Home from "./components/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
