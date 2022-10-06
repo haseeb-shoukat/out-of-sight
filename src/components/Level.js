9c471685e6d3a46012da55bb2389a5bd45db9b6d
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-10-06 18:43:57 +0500

    Add functionality to display menu + get coords

--- src/components/Level.js
+++ src/components/Level.js
@@ -1,4 +1,21 @@
+import { useState } from "react";
+
 const Level = ({ characters, image }) => {
+  const [menu, showMenu] = useState(false);
+  const [coords, setCoords] = useState([0, 0]);
+
+  const registerClick = (e) => {
+    const rect = e.target.getBoundingClientRect();
+    showMenu(true);
+    setCoords([e.clientX - rect.left, e.clientY - rect.top]);
+  };
+
+  const registerGuess = (e, key) => {
+    e.stopPropagation();
+    console.log(key);
+    console.log(coords);
+  };
+
   return (
     <div>
       <div className="nav-bar">
@@ -14,7 +31,29 @@ const Level = ({ characters, image }) => {
           })}
         </div>
       </div>
-      <img width="100%" src={image} />
+      <div className="main-container" onClick={registerClick}>
+        <img className="main-image" width="100%" src={image} />
+        {menu ? (
+          <div
+            className="menu"
+            style={{
+              position: "absolute",
+              top: `${coords[1] + 32}px`,
+              left: `${coords[0] + 32}px`,
+            }}
+          >
+            {characters.map((character) => (
+              <div
+                key={character.key}
+                className="menu-item"
+                onClick={(e) => registerGuess(e, character.key)}
+              >
+                {character.name}
+              </div>
+            ))}
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
