87fca7c6cbd9b4d5f0d69ba03b91aed06dacd314
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-10-06 05:58:43 +0500

    Add RouteSwitch component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style/index.css";
-import Home from "./components/Home";
+import RouteSwitch from "./components/RouteSwitch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <RouteSwitch />
   </React.StrictMode>
 );
