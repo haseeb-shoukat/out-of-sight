20d2a09e1e2e333d79d64963d680464682b05ed9
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-10-06 09:22:02 +0500

    Add new routes

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import RoboCity from "./RoboCity";
+import PostApocalypse from "./PostApocalypse";
+import UndergroundBase from "./UndergroundBase";
+import SpaceFrenzy from "./SpaceFrenzy";
 
 const RouteSwitch = () => {
   return (
@@ -8,6 +11,9 @@ const RouteSwitch = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/robo-city" element={RoboCity} />
+        <Route path="/post-apocalypse" element={PostApocalypse} />
+        <Route path="/underground-base" element={UndergroundBase} />
+        <Route path="/space-frenzy" element={SpaceFrenzy} />
       </Routes>
     </BrowserRouter>
   );
