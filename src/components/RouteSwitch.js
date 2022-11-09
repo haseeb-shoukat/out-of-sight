069bb7dbebcc7b9bf6ff39674bc900324cd8d15c
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-11-10 04:49:00 +0500

    Import HashRouter

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import RoboCity from "./RoboCity";
 import PostApocalypse from "./PostApocalypse";
